8ad4b2858bd185568817bdae12dc0bf7bb7c53af
christianekenstedt <christian>
2016-09-07 16:06:45 +0200

    added GameSession

---
+++ src/komsys/lab1A/GameSession.java
@@ -0,0 +1,26 @@
+package komsys.lab1A;
+
+import java.net.InetAddress;
+
+/**
+ * Created by chris on 2016-09-07.
+ */
+public class GameSession {
+    private InetAddress address;
+    private int port;
+
+    public GameSession(int port, InetAddress address){
+        this.port = port;
+        this.address = address;
+    }
+
+    public void start(){
+
+    }
+
+    public void guess(){
+
+    }
+
+
+}
